2acc3e7b25371ba3b52957b005ace09fec3baae2
Diego Carretero Alvarez <carretero.alv.diego@gmail.com>
2017-08-30 19:00:29 -0500

    Add Builder, Factory, Abstract Factory and Prototype Design Patterns

---
+++ BuilderDesignPattern/Exercise.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderDesignPattern
+{
+    public class CodeField
+    {
+        public string Name, Type;
+        private const int IndentSpace = 2;
+
+        public CodeField()
+        {
+
+        }
+
+        public CodeField(string name, string type)
+        {
+            this.Name = name;
+            this.Type = type;
+        }
+
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', IndentSpace * indent);
+            sb.AppendLine($"{i}public {Type} {Name};");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(1);
+        }
+    }
+
+    public class ClassField
+    {
+        public string Name;
+        public List<CodeField> Fields = new List<CodeField>();
+        private const int IndentSpace = 2;
+
+        public ClassField()
+        {
+
+        }
+        public ClassField(string name)
+        {
+            this.Name = name;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', IndentSpace * indent);
+            sb.Append($"{i}public class {Name}\n");
+            sb.Append("{\n");
+            foreach (var f in Fields)
+                sb.Append(f);
+            sb.Append("}");
+
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+
+    public class CodeBuilder
+    {
+        // TODO
+        private readonly string rootName;
+        ClassField root = new ClassField();
+
+        public CodeBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            var f = new CodeField(name, type);
+            root.Fields.Add(f);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new ClassField { Name = rootName };
+        }
+
+    }
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+//Inner Factory
+namespace FactoryDesignPattern
+{
+
+
+    public class Point
+    {
+        //factory method ctrl shift r
+
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double thetha)
+            {
+                return new Point(rho * Math.Cos(thetha), rho * Math.Sin(thetha));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+            WriteLine(point);
+
+            foreach (var m in MesHelper.ObtenerMeses())
+            {
+                WriteLine($"{m.NoMes} {m.NombreMes}");
+            }
+        }
+    }
+
+    public class MesHelper
+    {
+        public string NombreMes { get; set; }
+        public int NoMes { get; set; }
+
+        public MesHelper(string nombreMes, int noMes)
+        {
+            NombreMes = nombreMes;
+            NoMes = noMes;
+        }
+
+        public static List<MesHelper> ObtenerMeses()
+        {
+            List<MesHelper> meses = new List<MesHelper>();
+            for (int i = 1; i <= 12; i++)
+            {
+                string nombreMes = new DateTime(2000, i, 1).ToString("MMMM", CultureInfo.CreateSpecificCulture("es")).Capitalize();
+                meses.Add(new MesHelper(nombreMes, i));
+            }
+            return meses;
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string Capitalize(this string s)
+        {
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
+
